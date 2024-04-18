17ef2f23137dc41ee385ecceb25c81f2c8af52d9
Cris <c.cheuquel01@ufromail.cl>
2024-04-18 09:12:04 -0400

    generarSismo() entrega valores a los sismos generados aleatoreamente

---
+++ src/main/java/proyect/RegistroSismatologico.java
@@ -0,0 +1,13 @@
+package proyect;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+public class RegistroSismatologico {
+    ArrayList<Sismo> sismos = new ArrayList<Sismo>();
+    public void agregarSismo(Sismo sismo) {
+        sismos.add(sismo);
+    }
+    public Sismo generarSismo(){
+        // aqui se invoca el metodo para generar los datos del sismo de la rama rivas
+        return sismo;
+    }
+}
