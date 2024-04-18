ad23c405264405d5891d68eb515c6533d1db695a
Cris <c.cheuquel01@ufromail.cl>
2024-04-18 09:15:12 -0400

    llenarRegistro() rellena el arreglo RegistroSismatologico.registro

--- src/main/java/proyect/RegistroSismatologico.java
+++ src/main/java/proyect/RegistroSismatologico.java
@@ -2,12 +2,18 @@ package proyect;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class RegistroSismatologico {
-    ArrayList<Sismo> sismos = new ArrayList<Sismo>();
+    ArrayList<Sismo> registro = new ArrayList<Sismo>();
     public void agregarSismo(Sismo sismo) {
-        sismos.add(sismo);
+        registro.add(sismo);
     }
     public Sismo generarSismo(){
         // aqui se invoca el metodo para generar los datos del sismo de la rama rivas
         return sismo;
+    }
+    public void llenarRegistro(int cantidadSismos){
+        for(int i = 0; i < cantidadSismos; i++){
+            Sismo sismo = generarSismo();
+            agregarSismo(sismo);
+        }
     }
 }
